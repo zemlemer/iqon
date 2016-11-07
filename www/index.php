d2dd1667a9b1829e715f3d5813bea018d6b35b33
zemlemer <yglazov@gmail.com>
2016-11-07 15:53:01 +0300

    remove dd

--- www/index.php
+++ www/index.php
@@ -6,13 +6,6 @@ function env($key, $defaultValue)
 {
     return getenv($key) ?? $defaultValue;
 }
-function dd()
-{
-    foreach (func_get_args() as $call) {
-        var_dump($call);
-    }
-    exit;
-}
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
